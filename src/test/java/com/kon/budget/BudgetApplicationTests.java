e28ab872cd6800ddccb66063d50e2537d0fa88c2
akonowalskii@gmail.com <Polokipoloki123>
2021-12-13 20:28:57 +0100

    Init Commit

---
+++ src/test/java/com/kon/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kon.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
